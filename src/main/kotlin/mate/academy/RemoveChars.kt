3248c7fa874e7a033b717390eb1e7634431ef0a8
Сидоркін Андрій Володимирович <sydorkin-av@ukrposhta.ua>
2024-02-14 10:26:03 +0200

    task is implemented and tested

--- src/main/kotlin/mate/academy/RemoveChars.kt
+++ src/main/kotlin/mate/academy/RemoveChars.kt
@@ -1,6 +1,5 @@
 package mate.academy
 
-fun removeChars(str: String): String {
-    // Implement this function
-    return ""
-}
+fun removeChars(str: String): String = str.drop(CHARS_TO_DROP).dropLast(CHARS_TO_DROP)
+
+const val CHARS_TO_DROP = 1

7861930bbc806ed46b1fbafdc991bbf5a2f59e68
khorkovenko <khorkovenko>
2024-03-22 19:44:38 +0200

    done

--- src/main/kotlin/mate/academy/RemoveChars.kt
+++ src/main/kotlin/mate/academy/RemoveChars.kt
@@ -1,6 +1,9 @@
 package mate.academy
 
 fun removeChars(str: String): String {
-    // Implement this function
-    return ""
+    if (str.length <= 2) {
+        return ""
+    }
+
+    return str.substring(1, str.length - 1)
 }
